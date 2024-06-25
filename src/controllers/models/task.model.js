d1228818b68c462dd12af9da6e125ab3f6cf6e34
Joaquin <moralesjoaquin685@gmail.com>
2024-06-25 19:19:19 -0300

    Corregido el problema del get en users

--- src/controllers/models/task.model.js
+++ src/controllers/models/task.model.js
@@ -6,7 +6,7 @@ const TaskSchema = new Schema({
   name : String,
   description : String,
     resume : String,
-    user : String
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true }
 });
 
 module.exports = mongoose.model('Task', TaskSchema);
--- src/controllers/models/user.model.js
+++ src/controllers/models/user.model.js
@@ -5,7 +5,8 @@ const userSchema = new mongoose.Schema({
   firtname: String,
   lastname: String,
   documento:String,
-  role: String
+  role: String,
+  tasks: { type: Schema.Types.ObjectId, ref: 'Task' }
 });
 
 const userModel = mongoose.model("User", userSchema);
